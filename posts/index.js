bbf50682b8040c78167fa4e21821632c509b9084
kannan-swabhav <kannan@swabhavtechlabs.com>
2022-02-23 20:52:12 +0530

    dv2

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
 app.listen(4000,()=>{
-    console.log("hello..",new Date().toUTCString())
+    console.log("hello..dev2",new Date().toUTCString())
     console.log("listening on 4000")
 })
