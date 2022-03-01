a1836000f05e71284536ea896a11dc4f0d24325d
kannan-swabhav <kannan@swabhavtechlabs.com>
2022-03-01 12:52:53 +0530

    remaining services

--- query/index.js
+++ query/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 app.listen(4002,async ()=>{
-    console.log("listing in port 4002")
+    console.log("listing in port 4002..")
     const resp = await axios.get("http://"+LOCAL_HOST+":4005/events")
                           .catch(e=>console.log(e.message))
     if(resp){
