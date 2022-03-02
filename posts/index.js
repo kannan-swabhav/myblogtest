4e8a7841186421ea9a82a48af397ddd3e9c9bf4c
kannan-swabhav <kannan@swabhavtechlabs.com>
2022-03-02 11:10:21 +0530

    post ci change

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
 app.listen(4000,()=>{
-    console.log("hello..dev2..again111",new Date().toUTCString())
+    console.log("hello..dev2..again111.ci/cd",new Date().toUTCString())
     console.log("listening on 4000")
 })
